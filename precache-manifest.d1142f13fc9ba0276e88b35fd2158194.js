01df5579dc93f25a784966b7d15147019e4a7c57
Al <alex2015mig@gmail.com>
2020-11-07 02:14:19 +0200

    Updates

---
+++ precache-manifest.d1142f13fc9ba0276e88b35fd2158194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7962c31aa5c813048a2b1e1b2a94435",
+    "url": "/monster_dashboard/index.html"
+  },
+  {
+    "revision": "5954d2246cffe97f5d57",
+    "url": "/monster_dashboard/static/css/main.da93be61.chunk.css"
+  },
+  {
+    "revision": "623e34fadea66c319350",
+    "url": "/monster_dashboard/static/js/2.d25dd603.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/monster_dashboard/static/js/2.d25dd603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5954d2246cffe97f5d57",
+    "url": "/monster_dashboard/static/js/main.85f3a5c4.chunk.js"
+  },
+  {
+    "revision": "cb728ec8f5d55ba6de1a",
+    "url": "/monster_dashboard/static/js/runtime-main.591b948f.js"
+  }
+]);
